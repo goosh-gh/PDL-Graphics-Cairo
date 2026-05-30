5c4e1d6b7c59207a568cc12fb4adbea016a357aa
0000-0003-1286-9718 <zvb10512@nifty.com>
2026-05-31 04:17:16 +0900

    examples: update example_GraphViz.pl to use imread()

--- examples/example_GraphViz.pl
+++ examples/example_GraphViz.pl
@@ -1,9 +1,7 @@
 #!/usr/bin/env perl
 use strict; use warnings;
 use GraphViz2;
-use PDL;
-use PDL::IO::Pic;
-use PDL::Graphics::Cairo qw(figure);
+use PDL::Graphics::Cairo qw(figure imread);
 
 # --- 1. GraphViz2でグラフ生成 ---
 my $g = GraphViz2->new(
@@ -13,31 +11,23 @@ my $g = GraphViz2->new(
                 fillcolor => 'lightblue', fontname => 'Helvetica' },
     edge   => { color => 'gray40' },
 );
-
 my @x = qw(x1 x2 x3);
 my @h = qw(h1 h2 h3 h4);
 my @y = qw(y1 y2);
-
 $g->add_node(name => $_) for (@x, @h, @y);
 for my $xi (@x) { for my $h (@h) { $g->add_edge(from => $xi, to => $h) } }
 for my $h  (@h) { for my $y (@y) { $g->add_edge(from => $h,  to => $y) } }
-
 my $tmpfile = "/tmp/backprop_network.png";
 $g->run(format => 'png', output_file => $tmpfile);
 print "GraphViz2: saved $tmpfile\n";
 
 # --- 2. PDL::Graphics::Cairoで/osxに表示 ---
-my $img  = rpic($tmpfile);      # [3, W, H] uint8
-my $w_px = $img->dim(1);
-my $h_px = $img->dim(2);
+my $hwc = imread($tmpfile);     # [H,W,3] float32 upper-origin (自動正規化)
+my ($H, $W) = ($hwc->dim(0), $hwc->dim(1));
 
-my $fig = figure(width => $w_px, height => $h_px);
+my $fig = figure(width => $W, height => $H);
 my $ax  = $fig->axes();
-
-my $rgb = $img->float / 255.0;  # [3, W, H] → float
-my $hwc = $rgb->reorder(2,1,0); # [H, W, 3]
-$ax->imshow($hwc, origin => 'lower');
+$ax->imshow($hwc);              # origin指定不要
 $ax->axis('off');
-
 $fig->tight_layout();
 $fig->show(backend => 'osx');
